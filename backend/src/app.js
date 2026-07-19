f7dc9702076add81434c363dd76511bc18efe82d
Luciana Pillaca Ramos <pillacakatty08@gmail.com>
2026-07-18 21:11:46 -0500

    Corrige conexión del login del cocinero

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/authRoutes.js";
 import pedidoRoutes from "./routes/pedidoRoutes.js";
 
 const app = express();
 
-app.use(cors());
+const FRONTEND_URL =
+    process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(
+    cors({
+        origin: FRONTEND_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -16,6 +27,5 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/pedidos", pedidoRoutes);
-
 
 export default app;
